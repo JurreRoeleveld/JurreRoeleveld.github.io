e7a1b2c69fe40725c14e76fca6af95843d675e0c
Jurre Roeleveld <jurre.roeleveld@nedap.com>
2023-07-28 13:59:05 +0200

    hii

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  {console.log('blabla')}
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+console.log('hiii')
