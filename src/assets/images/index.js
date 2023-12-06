f70a7a61f7054f0a44cee1ac426c8cc6029acd40
Jurre Roeleveld <jurre.roeleveld@nedap.com>
2023-12-06 14:31:13 +0100

    remove unused images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,28 +1,11 @@
-import wijngaard from './wijngaard.jpg';
-
-import customer1 from './customer1.jpeg';
-import customer2 from './customer2.svg';
-
 import footerLogo from './footer-logo.svg';
 import headerLogo from './header-logo.svg';
 
 import offer from './offer.svg';
 
-import bottle1 from './bottle1.png';
-import bottle2 from './bottle2.png';
-import bottle3 from './bottle3.png';
-import bottle10 from './bottle10.png';
-import bottle20 from './bottle20.png';
-import bottle30 from './bottle30.png';
-
-import box1 from './box1.png';
-import box2 from './box2.png';
-import box3 from './box3.png';
-
 import nix18 from './nix-18.svg';
 
 import vineYard from './vineyard.jpg';
-import jesper from './jesper.jpg';
 import frank from './frank.jpg';
 import jesse from './jesse.jpg';
 
@@ -40,24 +23,11 @@ import pakket1 from './pakket1.jpg';
 import pakket2_1 from './pakket2_1.jpg';
 
 export {
-  customer1,
-  customer2,
   footerLogo,
   headerLogo,
   offer,
-  wijngaard,
-  bottle1,
-  bottle2,
-  bottle3,
-  bottle10,
-  bottle20,
-  bottle30,
-  box1,
-  box2,
-  box3,
   nix18,
   vineYard,
-  jesper,
   frank,
   gotas_de_plata,
   a,
