a36acfd24e493174117529c0b5deef5ab52404eb
Jurre Roeleveld <jurre.roeleveld@nedap.com>
2025-06-17 09:51:04 +0200

    include shadcn sidebar

---
+++ dublin2025/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+import Page from "./dashboard/page"
+
+
+function App() {
+
+  return (
+    <>
+      <Page />
+    </>
+  )
+}
+
+export default App
